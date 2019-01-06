837bbd1889c15b71b7dfaf7bc0a026119c547963
Aaron Barack <atbarck@gmail.com>
2019-01-06 14:20:41 -0800

    fixed some files and dirs

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ hbs.registerHelper('list', function(items, options) {
 
 app.get('/', (req, res) => {
 	console.log("User joined");
-	res.render(fs.readFileSync("public/home.html"));
+	res.render("public/home.html");
 });
 function get_content(title){
 	return fs.readFileSync("content/" + title+".html");
